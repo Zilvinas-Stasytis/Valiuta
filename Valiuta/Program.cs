0e509b8f5929968afc5c9743929baee175147110
Zilvinas-Stasytis <zalciukas@gmail.com>
2022-09-19 21:10:54 +0300

    Valiuta

--- Valiuta/Program.cs
+++ Valiuta/Program.cs
@@ -5,38 +5,44 @@ Console.WriteLine("1. Į JAV dolerius");
 Console.WriteLine("2. Į Lenkijos zlotus");
 Console.WriteLine("3. Į Baltarusijos rublius");
 Console.WriteLine("4. Pabaiga");
-string e;
 Console.WriteLine("Įveskite keičiamą sumą EUR");
-double euras = Console.ReadLine(Convert.ToDouble(e));
-string pasirinkimas;
-Console.ReadLine(pasirinkimas);
-switch (pasirinkimas) {
-    case "1": BuyingDollar(euras);break;
-    case "2": BuyingZlot(euras);break;
-    case "3": BuyingBYRouble(euras);break;
-    default: break;
+double Total;
+string e = Console.ReadLine();
+double euras = Convert.ToDouble(e);
+Console.WriteLine("Dabar įvesk pasirinkimą");
+string pasirinkimas = Console.ReadLine();
+double choose = Convert.ToInt32(pasirinkimas); 
+switch (choose)
+{
+    case 1: BuyingDollar(euras); break;
+    case 2: BuyingZlot(euras); break;
+    case 3: BuyingBYRouble(euras); break;
+    default:return;
 }
+
 void BuyingDollar(double euro){
     string c;
-    double Total;
     Console.WriteLine("Įveskite dolerio kurso koeficientą");
-    double course = Console.ReadLine(Convert.ToDouble(c));
+    c = Console.ReadLine();
+    double course = Convert.ToDouble(c);
     Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4) dollars", Total);
+    Console.WriteLine("Jums priklauso {0:F4} dollars", Total);
 }
 void BuyingZlot(double euro){
     string c;
     double Total;
-    Console.WriteLine("Įveskite dolerio kurso koeficientą");
-    double course = Console.ReadLine(Convert.ToDouble(c));
-    Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4) zlots", Total);
+    Console.WriteLine("Įveskite zloto kurso koeficientą");
+    c = Console.ReadLine();
+    double course = Convert.ToDouble(c);
+   Total = course * euro;
+    Console.WriteLine("Jums priklauso {0:F4} zlots", Total);
 }
 void BuyingBYRouble(double euro){
     string c;
     double Total;
     Console.WriteLine("Įveskite dolerio kurso koeficientą");
-    double course = Console.ReadLine(Convert.ToDouble(c));
+    c = Console.ReadLine();
+    double course = Convert.ToDouble(c);
     Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4) BYroubles", Total);
+    Console.WriteLine("Jums priklauso {0:F4} BYroubles", Total);
 }
