2311bbb49b68a39d95ddb5ed3f8c10ec188282bd
Zilvinas-Stasytis <zalciukas@gmail.com>
2022-09-20 18:55:26 +0300

    Valiuta

--- Valiuta/Program.cs
+++ Valiuta/Program.cs
@@ -7,42 +7,25 @@ Console.WriteLine("3. Į Baltarusijos rublius");
 Console.WriteLine("4. Pabaiga");
 Console.WriteLine("Įveskite keičiamą sumą EUR");
 double Total;
-string e = Console.ReadLine();
-double euras = Convert.ToDouble(e);
+double euras = Convert.ToDouble(Console.ReadLine());
+string currency;
 Console.WriteLine("Dabar įvesk pasirinkimą");
-string pasirinkimas = Console.ReadLine();
-double choose = Convert.ToInt32(pasirinkimas); 
+double choose = Convert.ToInt32(Console.ReadLine());
+if (choose == 4)
+{
+    return;
+}
+Console.WriteLine("Įvesk kurso koeficientą");
+double course = Convert.ToDouble(Console.ReadLine());
 switch (choose)
 {
-    case 1: BuyingDollar(euras); break;
-    case 2: BuyingZlot(euras); break;
-    case 3: BuyingBYRouble(euras); break;
-    default:return;
+    case 1: currency = "dollars"; Buying(euras,course,currency);  break;
+    case 2:currency = "zlotych"; Buying(euras, course, currency); break;
+    case 3: currency = "rubley"; Buying(euras, course, currency); break;
+    
 }
 
-void BuyingDollar(double euro){
-    string c;
-    Console.WriteLine("Įveskite dolerio kurso koeficientą");
-    c = Console.ReadLine();
-    double course = Convert.ToDouble(c);
-    Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4} dollars", Total);
-}
-void BuyingZlot(double euro){
-    string c;
-    double Total;
-    Console.WriteLine("Įveskite zloto kurso koeficientą");
-    c = Console.ReadLine();
-    double course = Convert.ToDouble(c);
-   Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4} zlots", Total);
-}
-void BuyingBYRouble(double euro){
-    string c;
-    double Total;
-    Console.WriteLine("Įveskite rublio kurso koeficientą");
-    c = Console.ReadLine();
-    double course = Convert.ToDouble(c);
-    Total = course * euro;
-    Console.WriteLine("Jums priklauso {0:F4} BYroubles", Total);
+void Buying(double euro,double cours,string geld){
+    Total = cours * euro;
+    Console.WriteLine("Jums priklauso {0:F4} {1}", Total,geld);
 }
